0afae7d4b46bebbdaeae818b311cbb7f043b3bf9
JCMaxWell12 <robertoherrera113@gmail.com>
2023-04-05 23:03:07 -0600

    get_songs() works

---
+++ lrc-downloader.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import os
+
+dir = '/home/rc/media0/audio/'
+
+
+def get_songs(dir):
+    """ return list of songs in the directory, only for audio files,
+    songs that already have a .lrc file are omitted"""
+    songs = list()
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            ext = os.path.splitext(file)[1]         # extension
+
+            if ext == '.mp3' or ext == '.flac':     # audio files
+                song_name = os.path.splitext(file)[0]
+                lrc_file = song_name + '.lrc'
+                lrc_file = os.path.join(root, lrc_file)
+
+                if not os.path.isfile(lrc_file):    # check if lrc file exists
+                    songs.append((lrc_file, song_name))
+
+    return songs
+
+
+for song in get_songs(dir):
+    print(type(song))
